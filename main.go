bc99e78e766bb69c42c6ed7fb6cd0d12bfd0d0a7
Manuel Barbaran <manuelbarbaran@hotmail.com>
2023-04-04 19:43:04 -0500

    siguinete commit

--- main.go
+++ main.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/godesde0/variables"
+	"runtime"
 )
 
 func main() {
 
-	estado, texto := variables.ConviertoaTexto(123456)
-	fmt.Println(estado)
-	fmt.Println(texto)
+	/*
+		estado, texto := variables.ConviertoaTexto(123456)
+		fmt.Println(estado)
+		fmt.Println(texto)
+	*/
+
+	if os := runtime.GOOS; os == "linux" || os == "OS X." {
+
+		fmt.Println("Esto no es Windows, es ", os)
+	} else {
+		fmt.Println("Esto es windows")
+
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
+
 }
