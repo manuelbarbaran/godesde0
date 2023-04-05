64d7e5cf8948f01baadf2f7e91c746bfd9907e98
Manuel Barbaran <manuelbarbaran@hotmail.com>
2023-04-04 20:36:10 -0500

    siguiente commit

--- main.go
+++ main.go
@@ -8,6 +8,28 @@ import (
 
 func main() {
 
+	/*
+		estado, texto := variables.ConviertoaTexto(123456)
+		fmt.Println(estado)
+		fmt.Println(texto)
+
+		if os := runtime.GOOS; os == "linux" || os == "OS X." {
+
+			fmt.Println("Esto no es Windows, es ", os)
+		} else {
+			fmt.Println("Esto es windows")
+
+		}
+
+		switch os := runtime.GOOS; os {
+		case "linux":
+			fmt.Println("Esto es Linux")
+		case "darwin":
+			fmt.Println("Esto es Darwin")
+		default:
+			fmt.Printf("%s \n", os)
+		}
+	*/
 	valorEntero, mensaje := ejercicios.VerificarEntero100("102")
 
 	fmt.Println("El valor entero es ", valorEntero)
